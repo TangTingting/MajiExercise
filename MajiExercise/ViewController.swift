2c25966f45552e30b24514d63cb21623d50a2341
tina <tangting606@163.com>
2020-09-20 13:47:15 +0800

    Initial Commit

---
+++ MajiExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MajiExercise
+//
+//  Created by Ting on 2020/9/20.
+//  Copyright © 2020 Ting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
