7806ed87f2410dd2df65e8dfcf25ec92a535e731
tina <tangting606@163.com>
2020-09-20 22:40:06 +0800

    add coreData to save history data

---
+++ MajiExercise/CoreDataManager/CoreDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataManager.swift
+//  MajiExercise
+//
+//  Created by Ting on 2020/9/20.
+//  Copyright © 2020 Ting. All rights reserved.
+//
+
+import UIKit
+
+class CoreDataManager: NSObject {
+
+}
---
+++ MajiExercise/Utils/MajiFunctions.swift
@@ -0,0 +1,13 @@
+//
+//  MajiFunctions.swift
+//  MajiExercise
+//
+//  Created by Ting on 2020/9/20.
+//  Copyright © 2020 Ting. All rights reserved.
+//
+
+import UIKit
+
+class MajiFunctions: NSObject {
+
+}
